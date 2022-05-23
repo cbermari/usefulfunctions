5637a4659084ab0ac02e6eb3e76fecb7336036a3
Celina BermudezArias <m.celina.bermudez@gmail.com>
2022-05-23 21:16:06 +0200

    distance function

---
+++ tests/test_distance.py
@@ -0,0 +1,16 @@
+# tests/test_distance.py
+from mlproject.distance import haversine
+import numpy as np
+
+def test_haversine_output_type():
+    #testing distance le Wagon to UVa
+    lat1, lon1 = 48.865070, 2.380009
+    lat2, lon2 = 41.6626268,-4.7099289
+    assert type(haversine(lon1, lat1, lon2, lat2)) == float
+
+def test_haversine_output_value():
+    #testing distance le Wagon to UVa
+    lat1, lon1 = 48.865070, 2.380009
+    lat2, lon2 = 41.6626268,-4.7099289
+    np.testing.assert_allclose(1070.3365819241662,
+                               haversine(lon1, lat1, lon2, lat2), rtol=1e-1)
